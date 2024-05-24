a2ed7edba6ef01743b95deaa5a18745554f1e6f6
hassamulhaq <developer.hassam@gmail.com>
2024-05-25 03:45:17 +0500

    Add transaction functional

--- src/components/ExpenseTracker/AddTransaction.vue
+++ src/components/ExpenseTracker/AddTransaction.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
 
+import {nanoid} from "nanoid";
+import {ref} from "vue";
+
+const emit = defineEmits([
+  'transactionSubmitted'
+]);
+
+const name = ref()
+const amount = ref()
+
 const addTransaction = () => {
-  alert('addTransaction')
+  if (!name.value || !amount.value) {
+    alert("Name and amount are required.")
+    return
+  }
+  let payloadTransaction = {
+    "id": nanoid(),
+    "name": name.value,
+    "amount": amount.value,
+  }
+  emit('transactionSubmitted', payloadTransaction);
+  name.value = null
+  amount.value = null
 }
 </script>
 
@@ -12,14 +33,22 @@ const addTransaction = () => {
       <form id="form" @submit.prevent="addTransaction">
         <div class="form-control">
           <label for="text">Text</label>
-          <input type="text" id="text" autocomplete="off" placeholder="Enter text..." />
+          <input
+              type="text"
+              id="text"
+              autocomplete="off"
+              placeholder="Enter text..." v-model="name"/>
         </div>
         <div class="form-control">
           <label for="amount"
-            >Amount <br />
+          >Amount <br/>
             (negative - expense, positive - income)</label
           >
-          <input autocomplete="off" type="text" id="amount" placeholder="Enter amount..." />
+          <input
+              autocomplete="off"
+              type="text"
+              id="amount"
+              placeholder="Enter amount..." v-model="amount"/>
         </div>
         <button class="add-transaction-btn">Add transaction</button>
       </form>
--- src/views/ExpenseTrackerView.vue
+++ src/views/ExpenseTrackerView.vue
@@ -4,7 +4,7 @@
     <Balance />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <<AddTransaction @transactionSubmitted="transactionSubmitted"/>
   </div>
 </template>
 
@@ -15,9 +15,10 @@ import IncomeExpenses from '@/components/ExpenseTracker/IncomeExpenses.vue'
 import TransactionList from '@/components/ExpenseTracker/TransactionList.vue'
 import AddTransaction from '@/components/ExpenseTracker/AddTransaction.vue'
 import Header from '@/components/ExpenseTracker/Header.vue'
-import { nanoid } from 'nanoid'
+import {nanoid} from 'nanoid'
+import {computed, ref} from "vue";
 
-const transactions = [
+let transactions = ref([
   {
     'id': nanoid(),
     'name': 'Pay Check',
@@ -25,23 +26,22 @@ const transactions = [
   },
   {
     'id': nanoid(),
-    'name': 'Lunch',
-    'amount': '-77'
-  },
-  {
-    'id': nanoid(),
-    'name': 'Fuel',
-    'amount': '-10'
-  },
-  {
-    'id': nanoid(),
-    'name': 'A4tech Mouse',
-    'amount': '-20'
-  },
-  {
-    'id': nanoid(),
-    'name': 'Bond',
-    'amount': '90'
+    'name': 'Hello',
+    'amount': '-40'
   }
-]
+]);
+
+
+function transactionSubmitted(data: any) {
+  if (data && data.hasOwnProperty('id') && data.hasOwnProperty('name') && data.hasOwnProperty('amount')) {
+    transactions.value.push(data);
+    return true;
+  }
+
+  alert('Error: Transaction');
+  return false;
+}
+
+
+
 </script>
