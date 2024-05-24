87a12b0e8bf7865a9498e3626371905426cb1494
hassamulhaq <developer.hassam@gmail.com>
2024-05-24 19:06:18 +0500

    Dummy data for expense-list and nanoid installed and UI tweaks

--- src/components/ExpenseTracker/TransactionList.vue
+++ src/components/ExpenseTracker/TransactionList.vue
@@ -1,16 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { nanoid } from 'nanoid'
+
+const transactionData = [
+  {
+    'id': nanoid(),
+    'name': 'Pay Check',
+    'amount': '400'
+  },
+  {
+    'id': nanoid(),
+    'name': 'Lunch',
+    'amount': '-77'
+  },
+  {
+    'id': nanoid(),
+    'name': 'Fuel',
+    'amount': '-10'
+  },
+  {
+    'id': nanoid(),
+    'name': 'A4tech Mouse',
+    'amount': '-20'
+  },
+  {
+    'id': nanoid(),
+    'name': 'Bond',
+    'amount': '90'
+  }
+]
+
+</script>
 
 <template>
   <div>
     <h3>History</h3>
     <ul id="list" class="list">
-      <li class="minus">
-        New <span>22</span>
-        <button class="delete-btn" @click="$emit('deleteExpenseRecord', '101')">x</button>
-      </li>
-      <li class="plus">
-        New <span>22</span>
-        <button class="delete-btn">x</button>
+      <li
+        v-for="transaction in transactionData"
+        :key="transaction.id"
+        :data-nanoid="transaction.id"
+        :class="(Number(transaction.amount) < 0) ? 'minus' : 'plus'">
+        {{ transaction?.name || 'N/A' }} <span>{{ Number(transaction?.amount) || '-' }}</span>
+        <button class="delete-btn" @click="$emit('deleteExpenseRecord', transaction.id)">x</button>
       </li>
     </ul>
   </div>
